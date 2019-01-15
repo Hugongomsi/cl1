20b01a409110b7011c1aaf5547f642cd00da63c3
Hugongomsi <45979673+Hugongomsi@users.noreply.github.com>
2019-01-15 19:37:56 +0200

    gitbash examen

---
+++ commands.sh
@@ -0,0 +1,17 @@
+mkdir folder
+touch./folder/blank
+ for me in 1 2 3 4 5 
+   do
+   echo "hello"geeting.txt
+   done
+     for i in 1 2 3 4 5 
+     do 
+     cat geeting.txt >>$i.txt
+      echo "cat">pets.txt
+      echo"dog">>pets.txt
+      echoe"hamster">>pets.txt
+      echo "cat">commands.txt
+      echo"ls">>commands.txt
+      echo"pwd">>commands.txt
+      
+      cat pets.txt commands.txt | sort| uniq>> lovelycommand.txt
